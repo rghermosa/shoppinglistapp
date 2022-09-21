380432cdd7770a1b9d7f34157f832a0cd0345009
Roger Gonzalez <rogergonzalezhermosa@gmail.com>
2022-09-21 12:46:02 +0200

    Solved problems + controller updated

--- src/Contexts/auth-management/Auth/infrastructure/typeorm/repositories/PostgresLoginRepository.ts
+++ src/Contexts/auth-management/Auth/infrastructure/typeorm/repositories/PostgresLoginRepository.ts
@@ -1,3 +1,5 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import { LoginRepository } from '../../../domain/interfaces/LoginRepository';
 import { User as UserDomain } from '../../../domain/User';
 import { UserMapper } from '../../UserMapper';
@@ -5,10 +7,21 @@ import { User } from '../entities/User';
 
 export class PostgresRegisterRepository implements LoginRepository {
   async login(email: string, password: string): Promise<any> {
-    return new Promise(async (resolve, reject) => {});
+    return new Promise(async (resolve, reject) => {
+      if (!this.exists(email)) {
+        throw new Error('User not found');
+      }
+      const user = await this.findToDomain(email);
+      if (bcrypt.compare(user.password, password)) {
+        return new Error('Password is not correct');
+      }
+      const token = jwt.sign({ user_id: user.id, email }, 'QWERTY', { expiresIn: '24h' });
+      const userFound = await User.findOneByOrFail({ email });
+      userFound.token = token;
+    });
   }
 
-  async find(email: string): Promise<UserDomain> {
+  async findToDomain(email: string): Promise<UserDomain> {
     return new Promise(async (resolve, reject) => {
       const user: UserDomain = await UserMapper.toDomain(User.findOneBy({ email }));
       resolve(user);
