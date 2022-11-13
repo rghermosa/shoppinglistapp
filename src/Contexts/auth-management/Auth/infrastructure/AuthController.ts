d20726adc7bb03b55426ed8dd489d788a42dcd41
Roger Gonzalez <rogergonzalezhermosa@gmail.com>
2022-11-13 18:18:29 +0100

    Solved compilation issues regarding user id

--- src/Contexts/auth-management/Auth/infrastructure/AuthController.ts
+++ src/Contexts/auth-management/Auth/infrastructure/AuthController.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import { container, injectable } from 'tsyringe';
-import { LoginController } from './LoginController';
 import { RegisterUserController } from './RegisterUserController';
 
 @injectable()
@@ -14,11 +13,6 @@ export class AuthController {
     this.router.post('/signup', (req, res) => {
       const registerUser = container.resolve(RegisterUserController);
       registerUser.run(req, res);
-    });
-
-    this.router.post('/login', (req, res) => {
-      const loginUser = container.resolve(LoginController);
-      loginUser.run(req, res);
     });
 
     return this.router;
