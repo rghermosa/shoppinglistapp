24a31644128a9d952e69f69157a465b76997e920
Roger Gonzalez <rogergonzalezhermosa@gmail.com>
2022-11-13 18:20:09 +0100

    Dependency resolver issue

--- src/Contexts/auth-management/app/routes/DependenciesResolver.ts
+++ src/Contexts/auth-management/app/routes/DependenciesResolver.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express';
 import { container, registry } from 'tsyringe';
 import { AuthController } from '../../Auth/infrastructure/AuthController';
-import { PostgresLoginRepository } from '../../Auth/infrastructure/typeorm/repositories/PostgresLoginRepository';
 import { PostgresRegisterRepository } from '../../Auth/infrastructure/typeorm/repositories/PostgresRegisterRepository';
 
 @registry([
-  { token: 'RegisterRepository', useClass: PostgresRegisterRepository },
-  { token: 'LoginRepository', useClass: PostgresLoginRepository },
+  { token: 'RegisterRepository', useClass: PostgresRegisterRepository }
 ])
 export class DependenciesResolver {
   public static run(router: Router) {
